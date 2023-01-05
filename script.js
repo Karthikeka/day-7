6b836a95994ab3a575d3519a1ba349f8d0881592
karthikeka <119919351+Karthikeka@users.noreply.github.com>
2023-01-05 17:21:49 +0530

    question 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Get all the countries from the Asia continent /region using the Filter function
+/ Print the following details name, capital, flag using forEach function
 
 const request=new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all")
@@ -7,8 +7,11 @@ request.onload=function(){
     if (request.status==200)
 {
     let c=JSON.parse(request.responseText)
-    c.filter((s)=>s.region==="Asia").map((e)=>{
-    console.log(e.name.official)})
+    c.forEach((g)=>{
+        console.log("Name:" + g.name.official);
+        console.log("capital:" + g.capital);
+        console.log("Flag:" + g.flags.svg);
+})
 }
     else   
 { 
